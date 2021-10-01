f759b077ee706383e39dd45ea7050e45d1155200
Paul-Chung27 <dnd8888@naver.com>
2021-10-01 14:06:43 +0900

    2021-10-01 Coding Test3 and its answers

--- Coding_Test3.py
+++ Coding_Test3.py
@@ -15,16 +15,30 @@ commands의 길이는 1 이상 50 이하입니다.
 commands의 각 원소는 길이가 3입니다.
 입출력 예
 array	commands	return
-[1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
+[1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
 """
-
 def solution(array, commands):
     answer = []
+    mid = []
     for i in range(len(commands)):
-        for j, d, k in commands[i][0],commands[i][1],commands[i][2]:
-            answer.append(array[j],array[d])
+        mid.append(sorted(array[commands[i][0]-1:commands[i][1]]))
+        print(mid)
+        print(commands[i][2])
 
+        answer.append(mid[i][commands[i][2]-1])
+        print(answer)
     return answer
 
 
-    [1, 5, 2, 6, 3, 7, 4]	[[2, 5, 3], [4, 4, 1], [1, 7, 3]]	[5, 6, 3]
+print(solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
+
+#다른 사람 답변
+def solution(array, commands):                                 
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))     
+
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i,j,k = command
+        answer.append(list(sorted(array[i-1:j]))[k-1])
+    return answer
