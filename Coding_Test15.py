6df7ff2739798c155746ca713dfe4fbd8d6a6c01
정성웅 <chung_sungwoong@jeongseong-ung-ui-MacBookPro.local>
2021-10-09 22:06:46 +0900

    2021-10-09 new

--- Coding_Test15.py
+++ Coding_Test15.py
@@ -10,3 +10,11 @@ def solution(s):
         answer += s[int((len(s)/2) - 1): int((len(s)/2) + 1)]
     return answer
 print(solution('abcdef'))
+
+def string_middle(str):
+    # 함수를 완성하세요
+
+    return str[(len(str)-1)//2:len(str)//2+1]
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(string_middle("power"))
