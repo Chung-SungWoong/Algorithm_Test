ae28344011c9955bd37a02f66a0a3d443cb06385
Chung SungWoong <dnd8888@naver.com>
2023-01-16 21:11:55 +0900

    Sort the string

---
+++ String_sort.py
@@ -0,0 +1,23 @@
+"""
+영어 대소문자로 이루어진 문자열 my_string이 매개변수로 주어질 때, 
+my_string을 모두 소문자로 바꾸고 알파벳 순서대로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요.
+"""
+def solution(my_string):
+    answer = ""
+    for i in my_string:
+        if ord(i) >= 65 and ord(i) <= 90:
+            answer += chr(ord(i) + 32)
+        else:
+            answer += i
+
+    answer = ''.join(sorted(answer))
+
+    return answer
+
+
+"""
+좋은 예시
+"""
+
+def solution(my_string):
+    return ''.join(sorted(my_string.lower()))
